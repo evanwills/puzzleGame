8fd2f305b45589b65b863a92a81c50555b4753ec
Evan Wills <evan.wills@acu.edu.au>
2015-09-10 11:38:03 +1000

    updated puzzlePiece.observer.class.php; alphabetised method declarations; added missing methods required by puzzlePiece interface.

--- puzzlePiece.observer.class.php
+++ puzzlePiece.observer.class.php
@@ -1,12 +1,12 @@
 <?php
 
-
+require_once(dirname(__FILE__).'/puzzle.interfaces.php');
 
 
 /**
  *
  */
-class puzzlePieceObserver implements puzzlePieceObserverInterface
+class puzzlePieceObserver implements puzzlePieceInterface , puzzlePieceObserverInterface
 {
 	static protected $traverse = true;
 	protected $piece = null;
@@ -15,8 +15,6 @@ class puzzlePieceObserver implements puzzlePieceObserverInterface
 	protected $boundary = false;
 	protected $x = -1;
 	protected $y = -1;
-
-	static protected $traverse = true;
 
 	public function __construct( puzzlePiece $piece , $neighbours ) {
 		$this->piece = $piece;
@@ -32,7 +30,7 @@ class puzzlePieceObserver implements puzzlePieceObserverInterface
 		if( !is_array($neighbours) ) {
 			$suffix = gettype($neighbours).' given';
 		} elseif( count($neighbours) !== $faceCount ) {
-			$suffix = 'array had '.count($neighbours).' items.'
+			$suffix = 'array had '.count($neighbours).' items.';
 		}
 		if( $suffix !== '' ) {
 			throw new exception ('puzzlePieceObserver::__construct() expects second parameter $neighbours to be an array containing '.$this->faceCount.' items. '.$suffix);
@@ -60,6 +58,25 @@ class puzzlePieceObserver implements puzzlePieceObserverInterface
 	}
 
 
+	public function getBridges() { return $this->piece->getBridges(); }
+
+	public function getCode() { return $this->piece->getCode(); }
+
+	public function getNeighbourObserver( $face ) {
+		if( is_int($face) && isset($this->neighbours[$face]) && get_class($this->neighbours[$a]) === get_class($this) ) {
+			return $this->neighbours[$a];
+		}
+		return false;
+	}
+
+	public function getOppositeFace($face) { return $this->piece->getOppositeFace($face); }
+
+	public function getOrientation() { return $this->piece->getOrientation(); }
+
+	public function getPieceClone() { return $this->piece->clone(); }
+
+	public function getPieceType() { return get_class($this->piece); }
+
 	public function getRequiredBridges() {
 		$output = array( 'count' => array('required' => 0 , 'optional' => 0 ) , 'bridges' => array() );
 		$half = ( $this->faceCount / 2 );
@@ -82,53 +99,22 @@ class puzzlePieceObserver implements puzzlePieceObserverInterface
 		return $output;
 	}
 
-
-	public function setPuzzlePiece( puzzlePiece $piece ) { $this->piece = $piece; }
-
-	public function isConnected() { return $this->piece->isConnected(); }
-
-	public function hasBridge( $face ) { return $this->piece->hasBridge($face); }
-
-	public function getOrientation() { return $this->piece->getOrientation(); }
-	
-	public function getPieceType() { return get_class($this->piece); }
-
-	public function rotate( $steps = 1 ) { return $this->piece->rotate($steps); }
-
-	public function rotateBack( $steps = 1 ) { return $this->piece->rotateBack($steps); }
-
 	public function getShape() { return $this->piece->getShape(); }
 
-	public function getCode() { return $this->piece->getCode(); }
-
-	public function isBoundary() { return $this->boundary; }
-	
 	public function getX() { return $this->X; }
 	public function getY() { return $this->Y; }
-	public function getY() { return $this->Y; }
+	public function getXY() { return array( 'x' => $this->X , 'y' => $this->Y ); }
 	public function getXYstr() { return $this->X.','.$this->Y; }
 
-	public function getNeighbourObserver( $face ) {
-		if( is_int($face) && isset($this->neighbours[$face] && get_class($this->neighbours[$a]) === get_class($this)) ) {
-			return $this->neighbours[$a];
-		}
-		return false;
-	}
-	
-	public function getPieceClone() {
-		return $this->piece->clone();
-	}
 
-	static public function blockTraversal() {
-		self::$traverse = false;
-	}
-	
-	protected function setNeighbourObserver( puzzlePieceObserver $piece , $face ) {
+	public function setNeighbourObserver( puzzlePieceObserver $piece , $face ) {
 		if( is_int($face) && isset($this->neighbours[$face]) && get_class($this->neighbours[$face]) === 'nullPuzzlePiece' ) {
 			$this->neighbours[$face] = $piece;
 		}
 	}
-	
+
+	public function setPuzzlePiece( puzzlePiece $piece ) { $this->piece = $piece; }
+
 	public function setXY( $x , $y ) {
 		if( is_int($x) && $x >= 0 && $this->X === -1 ) {
 			$this->X = $x;
@@ -141,4 +127,62 @@ class puzzlePieceObserver implements puzzlePieceObserverInterface
 			throw new exception(get_class($this).'::setXY() expects second parameter $Y to be an integer greater than or equal zero.');
 		}
 	}
+
+
+	public function connectToNeighbours() {
+
+		$neighbourBridges = array();
+		for( $a = 0 ; $a < $this->faceCount ; $a += 1 ) {
+			$neighbourBridges[$a] = $this->neighbours[$a]->hasBridge( $this->piece->getOppositeFace($a) );
+		}
+
+		$output = false;
+		$limit = $this->faceCount;
+		while( $output === false && $limit > 0 ) {
+			$unbridged = $this->bridgeCount;
+
+			// TODO this requires more thought
+			for( $a = 0 ; $a < $this->faceCount ; $a += 1 ) {
+				if( $neighbourBridges[$a] === $this->bridges[$a] || $neighbourBridges[$a] === null ) {
+					$unbridged -= 1;
+				}
+			}
+			if( $unbridged < 1 ) {
+				return true;
+			}
+			$this->piece->rotate();
+			$limit -= 1;
+		}
+
+		throw new exception('puzzlePiece::connectToNeighbours() cannot connect bridges to neighbours.');
+	}
+
+	public function isConnected() {
+
+		for( $a = 0 ; $a < $this->faceCount ; $a += 1 ) {
+			$neighbourBridge = $this->neighbours[$a]->hasBridge( $this->piece->getOppositeFace($a) );
+			if( $neighbourBridge !== $this->bridges[$a] && $neighbourBridge !== null ) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public function isBoundary() { return $this->boundary; }
+
+	public function hasBridge( $face ) { return $this->piece->hasBridge($face); }
+
+	public function rotate( $steps = 1 ) { return $this->piece->rotate($steps); }
+
+	public function rotate180() { return $this->piece->rotate180(); }
+
+	public function rotateBack( $steps = 1 ) { return $this->piece->rotateBack($steps); }
+
+	public function mirrorH() { return $this->piece->mirrorH(); }
+
+	public function mirrorV() { return $this->piece->mirrorV(); }
+
+	static public function StopBlockTraversal() { self::$traverse = false; }
+
+
 }
